ffa588b8ef3dec956e15772d8728d7480ef06632
typout31 <nicholas.chu98@gmail.com>
2021-04-04 14:05:40 -0700

    Create Javascript Notes and adjusted layout issues

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+/* dark-mode toggle button 
+
+1.   The dark-mode button is pressed and the background of the app will change to a dark theme.
+2.   The button is pressed again and the background will return to its original state
+
+*/
+
+/* New note save 
+
+New note button:
+    - the button will create a note taking area (textarea) and will have new buttons below it
+      for save and cancel
+         - clicking save will add it to notesArray
+    - clicking the save and cancel button will return the client to a screen without a note taking area
+
+-    new notes in the notesArray should display on the the list of notes with a note title on the side
+
+*/
+
+/* display note that is clicked 
+
+Note title:
+    - When a note title is clicked, the note is displayed in the main area
+    - A close button will appear which will remove the note from the main area and return
+      app to its original state
+
+*/
+
